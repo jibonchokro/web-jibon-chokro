005a0d20fddd61087e2cf033702130a32c92f096
Jahidul Haque Shuvo <jahidulhaque1921@gmail.com>
2026-07-12 03:40:55 +0600

    First Change

--- src/components/page/PageLayout.tsx
+++ src/components/page/PageLayout.tsx
@@ -13,7 +13,7 @@ export default function PageLayout({
     children,
 }: PageLayoutProps) {
     return (
-        <main className="mx-auto max-w-5xl px-4 pt-10 pb-0">
+        <main className="mx-auto max-w-5xl px-4 py-10">
 
             <PageHero
                 title={title}
